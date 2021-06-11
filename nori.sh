2839a629748ca3ce245f478c10e3cc6ca40c260b
Brian Kaney <brian@vermonster.com>
2021-06-11 18:41:44 -0400

    Update nori.sh

--- nori.sh
+++ nori.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -p 2021:2021 -it -v "$(pwd)":/workspace shorthand "$@"
+docker run -e JAVA_OPTS='-Xmx2048m -Xms2048m' -p 2021:2021 -it -v "$(pwd)":/workspace shorthand "$@"
