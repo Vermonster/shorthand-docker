f4898e3e994a9ef1af4fc9d1865bbc276bfd7f44
Brian Kaney <brian@vermonster.com>
2022-04-06 18:15:42 -0400

    Update nori.ps1
    Add support to set JAVA_OPTS

--- nori.ps1
+++ nori.ps1
@@ -1,9 +1,9 @@
 #requires -Version 3
-
+ 
 # TODO: Replace with HL7 once they update dockerhub
 # $image="hl7fhir/ig-publisher-base"
 $image="bkaney/ig-publisher-base:latest"
-
+ 
 function Invoke-InDocker {
     docker run --rm -it -v ${PWD}:/home/publisher/ig `
         -e JAVA_OPTS="-Xmx2048m -Xms2048m" `
@@ -13,16 +13,16 @@ function Invoke-InDocker {
         $image `
         $args
 }
-
+ 
 function Invoke-Publish {
     if (-not(Test-Path -Path $PWD\input-cache)) {
         Invoke-InDocker ./_updatePublisher.sh -f
     }
-
+ 
     Invoke-InDocker ./_genonce.sh
 }
-
-
+ 
+ 
 function Set-CacheDirectories {
     if (-not(Test-Path $HOME\.ig-publisher-cache\.gradle)) {
             mkdir -p $HOME\.ig-publisher-cache\.gradle
@@ -34,7 +34,7 @@ function Set-CacheDirectories {
             mkdir -p $HOME\.ig-publisher-cache\.node
     }
 }
-
+ 
 function Reset-CacheDirectories {
     if (Test-Path $HOME\.ig-publisher-cache) {
         rm -r "$Home\.ig-publisher-cache"
@@ -45,49 +45,54 @@ function Reset-CacheDirectories {
     docker pull $image
     Set-CacheDirectories
 }
-
-
+ 
+function Set-JavaOpts {
+    if (Test-Path $PWD\_genonce.sh) {
+        ((Get-Content -path $PWD\_genonce.sh -Raw) -replace 'java -jar','java $JAVA_OPTS -jar') | Set-Content -Path $PWD\_genonce.sh
+    }
+}
+ 
+ 
 # Program Flow...
-
-$param1=$args[0]
-
 Set-CacheDirectories
-
+Set-JavaOpts
+ 
+$param1=$args[0]
 if ($param1 -match "publish") {
     Invoke-Publish
 } elseif ($param1 -match "reset") {
     Reset-CacheDirectories
 } elseif ($param1 -match "(help|h)$") {
     $help = @"
-
+ 
 Usage: nori <COMMAND>
-
+ 
 A wrapper around IG Publisher and related tools.
-
+ 
 --
-
+ 
 Commands:
 publish:  runs the IG publisher
 sushi:    runs the sushi command
 reset:    rest local cache and docker image
-
+ 
 NOTE: You can also run any command relative to your IG root, e.g.
-
+ 
 > nori ./_updatePublisher.sh
-
+ 
 --
-
+ 
 Examples:
-
+ 
 > nori sushi --init
 > nori sushi --version
 > nori sushi
 > nori publish
-
+ 
 "@
-
+ 
     echo $help
-
+ 
 } else {
     Invoke-InDocker @args
 }
